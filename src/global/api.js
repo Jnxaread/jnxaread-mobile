6c0811f951cd53304613343e22dfb57c93390103
李嵩 <lisong@pansoft.com>
2020-07-16 10:03:03 +0800

    所有组件已准备就绪，即将开始开发

---
+++ src/global/api.js
@@ -0,0 +1,36 @@
+export default {
+    user: {
+        signIn: '/user/signIn',
+        signUp: '/user/signUp',
+        logout: '/user/logout',
+        editBaseInfo: '/user/edit/baseInfo',
+        editPassword: '/user/edit/password',
+        emailCode: '/user/emailCode',
+    },
+    library: {
+        fictions: '/library/list/fiction',
+        chapters: '/library/list/chapter',
+        comments: '/library/list/comment',
+        categories: '/library/list/category',
+        latestFictions: '/library/list/fiction/latest',
+        fictionDetail: '/library/detail/fiction',
+        chapterDetail: '/library/detail/chapter',
+        chapterNumber: '/library/detail/chapter/number',
+        fictionBrief: '/library/brief/fiction',
+        chapterBrief: '/library/brief/chapter',
+        newFiction: '/library/new/fiction',
+        newChapter: '/library/new/chapter',
+        newComment: '/library/new/comment',
+        editChapter: '/library/edit/chapter',
+        hideChapter: '/library/hide/chapter',
+        deleteChapter: '/library/delete/chapter',
+    },
+    forum: {
+        notices: '/forum/list/notice',
+        topics: '/forum/list/topic',
+        latestTopics: '/forum/list/topic/latest',
+        topicDetail: '/forum/detail/topic',
+        newTopic: '/forum/new/topic',
+        newReply: '/forum/new/reply',
+    }
+};
--- src/main.js
+++ src/main.js
@@ -2,11 +2,44 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import qs from 'qs';
+import moment from 'moment';
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import global from './global/global.js'
+import API from './global/api'
 
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+Vue.use(global);
+Vue.use(Vant);
+Vue.prototype.qs = qs;
+
+Vue.prototype.api = API;
+Vue.config.productionTip = false;
+
+Vue.filter('dateFormat', function (tick) {
+    return moment(tick).format("YYYY-MM-DD HH:mm:ss");
+});
+
+//开发环境
+axios.defaults.baseURL = 'http://localhost:6001';
+//生产环境
+//axios.defaults.baseURL = 'http://inhive.net/server';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.withCredentials = true;
+
+// http request 拦截器
+axios.interceptors.request.use(config => {
+    config.data = qs.stringify(config.data);
+    return config;
+}, error => {
+    return Promise.reject(error)
+});
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
